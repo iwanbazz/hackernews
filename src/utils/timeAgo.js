0c6993cbe98540285659770ff6501998e5d5aa52
iwanbazz <iwan.bazz@gmail.com>
2021-03-14 11:22:30 +0700

    Create time converter

---
+++ src/utils/timeAgo.js
@@ -0,0 +1,23 @@
+export default function timeAgo(data) {
+  const formatter = new Intl.RelativeTimeFormat('id', {
+    numeric: 'auto',
+  })
+  const DIVISIONS = [
+    { amount: 60, name: 'seconds' },
+    { amount: 60, name: 'minutes' },
+    { amount: 24, name: 'hours' },
+    { amount: 7, name: 'days' },
+    { amount: 4.34524, name: 'weeks' },
+    { amount: 12, name: 'months' },
+    { amount: Number.POSITIVE_INFINITY, name: 'years' },
+  ]
+  let duration = (data - new Date()) / 1000
+
+  for (let i = 0; i <= DIVISIONS.length; i++) {
+    const division = DIVISIONS[i]
+    if (Math.abs(duration) < division.amount) {
+      return formatter.format(Math.round(duration), division.name)
+    }
+    duration /= division.amount
+  }
+}
